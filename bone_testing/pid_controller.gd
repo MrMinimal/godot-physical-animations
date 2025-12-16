8920e7bc848f729b56208a344ec0c6f78b5f27cc
Tom Langwaldt <t.langwaldt@gmail.com>
2025-12-16 22:48:00 +0100

    Change system to be force dependent

--- bone_testing/pid_controller.gd
+++ bone_testing/pid_controller.gd
@@ -1,22 +1,26 @@
 extends Generic6DOFJoint3D
 @onready var marker_3d: Marker3D = $Marker3D
 
+const P_GAIN = 1
+
 func _physics_process(delta: float) -> void:
 	# Convert basis to quaternion, keep in mind scale is lost
 	var a = Quaternion(marker_3d.basis)
 	var b = Quaternion(get_node(node_b).basis)
 	# Interpolate using spherical-linear interpolation (SLERP).
 	#var c = a.slerp(b, 0.2)
-	var offset = b * a.inverse()
+	var error = b * a.inverse()
 	
 	var output: Vector3 = Vector3(
-		rad_to_deg(offset.get_euler().x),
-		rad_to_deg(offset.get_euler().y),
-		rad_to_deg(offset.get_euler().z),
+		rad_to_deg(error.get_euler().x),
+		rad_to_deg(error.get_euler().y),
+		rad_to_deg(error.get_euler().z),
 	)
 	print(output)
 
-	if offset.x > 0:
-		set("angular_motor_x/target_velocity", 2)
+	var P = P_GAIN * error.x
+
+	if error.x > 0:
+		set("angular_motor_x/target_velocity", 2000)
 	else:
-		set("angular_motor_x/target_velocity", -2)
+		set("angular_motor_x/target_velocity", -2000)
