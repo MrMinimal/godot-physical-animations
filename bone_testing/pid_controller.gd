e1cba64fb282adb3e29b2a7d3116f3eb11922bfa
Tom Langwaldt <t.langwaldt@gmail.com>
2025-12-16 22:28:20 +0100

    Fix model rotations

--- bone_testing/pid_controller.gd
+++ bone_testing/pid_controller.gd
@@ -1,19 +1,22 @@
 extends Generic6DOFJoint3D
-@onready var marker_3d: Marker3D = $"../../../../Marker3D"
+@onready var marker_3d: Marker3D = $Marker3D
 
 func _physics_process(delta: float) -> void:
-	var test = get_node(node_b).transform.affine_inverse() * self.global_transform
-	#print(rad_to_deg(test.basis.get_euler().x))
-	#print(rad_to_deg(test.basis.get_euler().y))
-	#print(rad_to_deg(test.basis.get_euler().z))
-	
-		# Convert basis to quaternion, keep in mind scale is lost
+	# Convert basis to quaternion, keep in mind scale is lost
 	var a = Quaternion(marker_3d.basis)
 	var b = Quaternion(get_node(node_b).basis)
 	# Interpolate using spherical-linear interpolation (SLERP).
 	#var c = a.slerp(b, 0.2)
 	var offset = b * a.inverse()
 	
-	print(offset.get_euler()) # in radians
+	var output: Vector3 = Vector3(
+		rad_to_deg(offset.get_euler().x),
+		rad_to_deg(offset.get_euler().y),
+		rad_to_deg(offset.get_euler().z),
+	)
+	print(output)
 
-	set("angular_motor_x/target_velocity", offset.x * 5)
+	if offset.x > 0:
+		set("angular_motor_x/target_velocity", 1)
+	else:
+		set("angular_motor_x/target_velocity", -1)
