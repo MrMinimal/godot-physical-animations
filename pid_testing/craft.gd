b707bc67450c4291d60c79c892fe3704107f3c2e
Tom Langwaldt <t.langwaldt@gmail.com>
2025-12-22 11:23:24 +0100

    Add working P and D term

--- pid_testing/craft.gd
+++ pid_testing/craft.gd
@@ -1,17 +1,42 @@
 extends RigidBody3D
 
+const MAX_FORCE = 10
+
 @export
 var targets: Array[Node3D]
 
-@export_enum("1", "2", "3") var selected: int
+@export_enum("1", "2", "3")
+var selected: int
 
-@export
-var p_gain: float
+var pFactor = 1
+var iFactor = 0
+var dFactor = 20
+
+var lastError = 0
+var integralActiveZone = 50
+
 
 func _physics_process(delta: float) -> void:
-	var error: float =  self.position.x - targets[selected].position.x
-	var p: float= p_gain * error;
-	error = clampf(error, -1, 1)
+	var R = self.position.x
+	var Y = targets[selected].position.x
+	var kp = 0
+	var ki = 0
+	var kd = 0
+	var time = delta
+	var currentError = R - Y
 	
-	apply_force(Vector3(-error * 10, 0, 0))
-	print(error)
+	if (currentError != 0):
+		kp = currentError * pFactor
+		ki = iFactor + currentError * pFactor
+	else:
+		ki = 0
+		kp = 0
+ 
+	if (currentError != 0):
+		kd = ((currentError - lastError)) * dFactor
+	else:
+		kd = 0
+	lastError = currentError
+	var out = kp + ki + kd
+	
+	apply_force(Vector3(-out * MAX_FORCE, 0, 0))
